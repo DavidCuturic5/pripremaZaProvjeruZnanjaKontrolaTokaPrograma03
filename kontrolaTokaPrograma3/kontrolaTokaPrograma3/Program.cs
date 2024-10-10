2da1fa460933d152657d5b02e45c6194df83aa48
DavidCuturic5 <david.cuturic5@gmail.com>
2024-10-10 15:16:24 +0200

    Add files via upload

---
+++ kontrolaTokaPrograma3/kontrolaTokaPrograma3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace kontrolaTokaPrograma3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x,y;
+
+            Console.WriteLine("Unesite cijenu sa PDV-om:");
+             x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Unesite cijenu bez PDV-a:");
+             y = Convert.ToDouble(Console.ReadLine());
+
+
+
+            double iznosPDV = x - y;
+            double postotakPDV = (iznosPDV / y) * 100;
+
+            Console.WriteLine($"Iznos PDV-a: {iznosPDV}");
+            Console.WriteLine($"Postotak PDV-a: {postotakPDV}%");
+                
+            Console.ReadKey();
+
+
+        }
+    }
+}
